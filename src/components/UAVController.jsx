c3e361728152886c1e5d472d6f025b090f10860f
JastejS28 <143274441+JastejS28@users.noreply.github.com>
2025-07-23 15:03:55 +0530

    Updated UAVController.jsx

--- src/components/UAVController.jsx
+++ src/components/UAVController.jsx
@@ -1,5 +1,5 @@
 // If you don't have this file, create it or add to your existing animation loop
-
+import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber';
 import { useUAVStore } from '../store/uavStore';
 import { useAttackDroneStore } from '../store/attackDroneStore';
